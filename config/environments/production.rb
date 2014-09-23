bcd9ca93b573f4bbc856c9368b3b160a13a98a86
Vincent Fleischacker <vdynmx@gmail.com>
2014-09-23 03:59:20 +0000

    Configuredd app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Bloccit::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -77,4 +78,5 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'vdynmx-bloccit.herokuapp.com' }
 end
