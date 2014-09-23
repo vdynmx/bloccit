5fffb3854e8cf00d334c14d0e62d03fc3bab829b
Vincent Fleischacker <vdynmx@gmail.com>
2014-09-23 20:58:40 +0000

    Refactored Post and Topics Controller post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     @post.topic = @topic
     authorize @post
 
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize @post
 
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
@@ -44,4 +44,9 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  private
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
 end
