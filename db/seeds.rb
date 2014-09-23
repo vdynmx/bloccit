215dff107a951186cdd82e0eb0f1e808e6c00ac7
Vincent Fleischacker <vdynmx@gmail.com>
2014-09-23 03:39:17 +0000

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,20 @@ require 'faker'
 end
 users = User.all
 
-# Note: by calling `User.new` instead of `create`,
-# we create an instance of User which isn't immediately saved to the database.
-
-# The `skip_confirmation!` method sets the `confirmed_at` attribute
-# to avoid triggering an confirmation email when the User is saved.
-
-# The `save` method then saves this User to the database.
-
+# Create Topics
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
 
 # Create Posts
 50.times do
   Post.create!(
     user:   users.sample,
+      topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -40,10 +41,34 @@ posts = Post.all
   )
 end
  
- User.first.update_attributes!(
-  email: 'vdynmx@aol.com',
-  password: '12345678',
-)
+  # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
