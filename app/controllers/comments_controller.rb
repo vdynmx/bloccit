c5d402a64055fb5ab86f57636df3344f994d37f5
Vincent Fleischacker <vdynmx@gmail.com>
2014-09-29 01:35:58 +0000

    Added Ajax destroy and fixed post comment view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+  respond_to :html, :js
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
@@ -28,11 +28,13 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldnt be deleted try again."
-      redirect_to [@topic, @post]
     end
+
+    respond_with(@comment) do |format|
+       format.html { redirect_to [@post.topic, @post] }
+     end
   end
   
 end
