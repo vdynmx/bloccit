e1fbdb1781d371a1506921dc816860300d322569
Vincent Fleischacker <vdynmx@gmail.com>
2014-11-04 15:00:13 +0000

    removed falsenamed controller file

--- app/controllers/post_controller.rb
+++
@@ -1,5 +0,0 @@
-class PostsController < ApplicationController
-   def index
-     @posts = Post.visible_to(current_user).where("posts.created_at > ?", 7.days.ago).paginate(page: params[:page], per_page: 10)
-   end
- end
