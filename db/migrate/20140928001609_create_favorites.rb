cc84229f241518a13bdf26ca6f235f8257ab44d0
Vincent Fleischacker <vdynmx@gmail.com>
2014-11-04 19:00:57 +0000

    Removed index from references

--- db/migrate/20140928001609_create_favorites.rb
+++ db/migrate/20140928001609_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
-      t.references :post, index: true
-      t.references :user, index: true
+      t.references :post
+      t.references :user
 
       t.timestamps
     end
